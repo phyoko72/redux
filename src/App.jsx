10be9fa4bb0b4c13c7402e592ad42f8d76faee5e
phyokoko777 <pkkoedu777@gmail.com>
2023-01-16 23:52:34 +0630

    using hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
-import { connect } from 'react-redux'
+import { connect, useDispatch, useSelector } from 'react-redux'
 import { increment, decrement } from '../reduxInReact'
 import './App.css'
 
 function App(props) {
 
-  console.log("Props: ",props);
+   const count = useSelector(state=>state)
+
+  const dispatch = useDispatch()
 
   return (
     <>
       <h1>App Page</h1>
-      <h1>Count: {props.count} </h1>
-      <button onClick={()=>props.decrement(1)}>-</button>
-      <button onClick={()=>props.increment(1)}>+</button>
+      <h1>Count: {count} </h1>
+      <button onClick={()=>dispatch(decrement(1))}>-</button>
+      <button onClick={()=>dispatch(increment(1))}>+</button>
     </>
   )
 }
 
-function mapStateToProps(globalState){
-  return { 
-    count : globalState
-  }
-}
+// function mapStateToProps(globalState){
+//   return { 
+//     count : globalState
+//   }
+// }
 
-const mapDispatchToProps = {
-  increment,
-  decrement
-}
+// const mapDispatchToProps = {
+//   increment,
+//   decrement
+// }
 
-export default connect(mapStateToProps,mapDispatchToProps)(App)
+// export default connect(mapStateToProps,mapDispatchToProps)(App)
+export default App
+
+// https://thoughtbot.com/blog/using-redux-with-react-hooks
+// https://react-redux.js.org/api/hooks#usage-warnings
